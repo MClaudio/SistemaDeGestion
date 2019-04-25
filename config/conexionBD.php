0f3e00cf0a4b82471882cbf8e4d4b610fef81502
Claudio <claum0395@gmail.com>
2019-04-25 16:26:47 -0500

    se crea el arcgchivo conexionDB

---
+++ config/conexionBD.php
@@ -0,0 +1,10 @@
+
+<?php
+$servename="localhost";
+$username="root";
+$password="";
+$dbname="hipermedial";
+
+$conn=new mysqli($servename,$username,$password,$dbname);
+mysqli_set_charset($conn, "utf8");
+?>
