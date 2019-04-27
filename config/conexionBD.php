276cf787d387cd2748592e4716701fd1c7ce9dce
Claudio <claum0395@gmail.com>
2019-04-27 09:13:53 -0500

    se crea el archivo de formulario.

--- config/conexionBD.php
+++ config/conexionBD.php
@@ -1,10 +1,9 @@
-
 <?php
-$servename="localhost";
-$username="root";
-$password="";
-$dbname="hipermedial";
+$servename = "localhost";
+$username = "root";
+$password = "";
+$dbname = "hipermedial";
 
-$conn=new mysqli($servename,$username,$password,$dbname);
+$conn = new mysqli($servename, $username, $password, $dbname);
 mysqli_set_charset($conn, "utf8");
 ?>
