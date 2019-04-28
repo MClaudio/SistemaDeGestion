94e6baa9b6e0a749655df0e84905124c2d5810a0
Claudio <claum0395@gmail.com>
2019-04-28 16:29:44 -0500

    Se corrigen errores

--- admin/controladores/deleteUser.php
+++ admin/controladores/deleteUser.php
@@ -1,18 +1,15 @@
 <?php
     include'../../config/conexionBD.php';
     $cod= isset($_GET["usu_cod"]) ? trim($_GET["usu_cod"]):null;             
-    //$cod=$row["usu_codigo"];
     $delete = isset($_GET["delete"]) ? trim($_GET["delete"]):null;
-    $delete;
+
     if($cod!=null and $delete==true){
         $sql= "UPDATE usuario SET usu_eliminado='S' WHERE usu_codigo='$cod';";             
         $result = $conn->query($sql);
-        $delete;
         header("Location: ../vista/usuario/index.php");
     }elseif($cod!=null and $delete==false){
         $sql= "UPDATE usuario SET usu_eliminado='N' WHERE usu_codigo='$cod';";             
         $result = $conn->query($sql);
-        $delete;
         header("Location: ../vista/usuario/index.php");
     }else{
         header("Location: ../vista/usuario/index.php");
